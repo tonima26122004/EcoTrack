8dbbfa6acaa6e68351278ef8eef376866aeae568
tonima <tonimadas348@gmail.com>
2025-03-29 12:00:18 +0530

    chat

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -18,14 +18,14 @@ const Chat = () => {
     <div className='bg-[#C0F2CB] h-screen font-lato'>
       <Nav />
       <div className='bg-[#F1FCF3] rounded-2xl h-[85%] w-[97%] mx-auto flex flex-col items-center justify-center relative'>
-
+{/* 
         {showChatDisplay ? (
           <Chat_display ans={ans} displaybutton={true} />
-        ) : (
+        ) : ( */}
           <div className='flex flex-col items-center text-center w-full max-w-[45%] main_content'>
             <div className="flex flex-col items-center gap-2">
               <h1 className='text- text-[#575B58]'>Welcome to EcoTalk</h1>
-              <h1 className='text-6xl font-semibold'>What Eco-Friendly Solution <br />
+              <h1 className='text-5xl font-semibold'>What Eco-Friendly Solution <br />
                 Do You Need?</h1>
               <h1 className='text-lg px-3 mt-2 text-[#575B58]'>
                 Choose a category to access in-depth analysis, gain valuable insights, 
@@ -35,16 +35,16 @@ const Chat = () => {
               </h1>
             </div>
 
-            <div className='flex justify-center gap-6 mt-8'>
+            <div className='flex justify-center gap-6 mt-8 '>
               <Card imageSrc="https://via.placeholder.com/150" heading="Carbon Footprint " />
               <Card imageSrc="https://via.placeholder.com/150" heading="Plant-Based Remedies" />
               <Card imageSrc="https://via.placeholder.com/150" heading="Sustainable Living" />
             </div>
           </div>
-        )}
+        
 
         <div className='absolute bottom-4 w-[97%]'>
-          <AnimatedInputBox getans={handleSendClick} />
+          {/* <AnimatedInputBox getans={handleSendClick} /> */}
         </div>
       </div>
     </div>
