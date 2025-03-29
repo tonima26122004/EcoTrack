31a777902e1b65b63e7a0f3f15c321b7fe3a2e4b
samprit004 <sampritdas2004@gmail.com>
2025-03-29 23:38:22 +0530

    fix

--- src/components/trail/trail.jsx
+++ src/components/trail/trail.jsx
@@ -3,7 +3,7 @@ import Nav from '../../Nav';
 import Display from './display';
 function Trail() {
   return (
-    <div className='bg-[#C0F2CB] max-h-[85vh] font-lato px-4'>
+    <div className='bg-[#C0F2CB] max-h-screen font-lato px-4'>
       <Nav />
       <Display />
     </div>
